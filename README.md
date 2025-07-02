088aeb20d9cee00356e196b921292f4ca983ef07
jimmyliow <yijun.liao@gmail.com>
2025-07-02 09:27:56 +0800

    initial project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Get started
+
+Dictation Gym.
+
+## Deployment
+
+flutter build apk --split-per-abi
+
+flutter build windows
---
+++ android/app/src/main/kotlin/com/jimmyliow/dictation_gym/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.jimmyliow.dictation_gym
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
